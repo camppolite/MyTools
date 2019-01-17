ff1f565627d53b7e9e17345ecbc7f6df93ac1a2f
camppolite <camppolite@gmail.com>
2019-01-17 15:54:05 +0800

    dsadas

--- QtEZTools/killprocees.py
+++ QtEZTools/killprocees.py
@@ -10,27 +10,34 @@ p = subprocess.Popen("TASKKILL /IM  %s /F" % "CanvaExecute.exe", shell=True, std
 print("kill process:", p.communicate()[0].decode("utf-8"))
 p = subprocess.Popen("TASKKILL /IM  %s /F" % "DbusDaemon.exe", shell=True, stdout=subprocess.PIPE)
 print("kill process:", p.communicate()[0].decode("utf-8"))
-p = subprocess.Popen("TASKKILL /IM  %s /F" % "dbus-daemon.exe", shell=True, stdout=subprocess.PIPE)
-print("kill process:", p.communicate()[0].decode("utf-8"))
+subprocess.Popen("TASKKILL /IM  %s /F" % "dbus-daemon.exe", shell=True, stdout=subprocess.PIPE)
+print("kill process:")
+subprocess.Popen("TASKKILL /IM  %s /F" % "qtez.demo.car.exe", shell=True, stdout=subprocess.PIPE)
+print("kill process:")
+subprocess.Popen("TASKKILL /IM  %s /F" % "qtez.demo.controller.exe", shell=True, stdout=subprocess.PIPE)
+print("kill process:")
+subprocess.Popen("TASKKILL /IM  %s /F" % "qtez.demo.calendar.exe", shell=True, stdout=subprocess.PIPE)
+print("kill process:")
+subprocess.Popen("TASKKILL /IM  %s /F" % "qtez.demo.chart.exe", shell=True, stdout=subprocess.PIPE)
+print("kill process:")
+
+#def fun1():
+#    name = "getserver"
+#    for i in range(1,10):
+#        p = subprocess.Popen("TASKKILL /IM  %s /F" % (name + str(i) + ".exe"), shell=True, stdout=subprocess.PIPE)
+#        print("kill process:", p.communicate()[0].decode("utf-8"))
 
 
-def fun1():
-    name = "getserver"
-    for i in range(1,10):
-        p = subprocess.Popen("TASKKILL /IM  %s /F" % (name + str(i) + ".exe"), shell=True, stdout=subprocess.PIPE)
-        print("kill process:", p.communicate()[0].decode("utf-8"))
+#def fun2():
+#    name = "servers"
+#    for i in range(1,10):
+#        p = subprocess.Popen("TASKKILL /IM  %s /F" % (name + str(i) + ".exe"), shell=True, stdout=subprocess.PIPE)
+#        print("kill process:", p.communicate()[0].decode("utf-8"))
 
-
-def fun2():
-    name = "servers"
-    for i in range(1,10):
-        p = subprocess.Popen("TASKKILL /IM  %s /F" % (name + str(i) + ".exe"), shell=True, stdout=subprocess.PIPE)
-        print("kill process:", p.communicate()[0].decode("utf-8"))
-
-t1 = threading.Thread(target=fun1, name="Thread-1")
-t2 = threading.Thread(target=fun2, name="Thread-2")
-t1.start()
-t2.start()
-t1.join()
-t2.join()
+#t1 = threading.Thread(target=fun1, name="Thread-1")
+#t2 = threading.Thread(target=fun2, name="Thread-2")
+#t1.start()
+#t2.start()
+#t1.join()
+#t2.join()
 
